7ef233db154673e65848602174a32cbeab27d5d0
Denys Khlivnyy <testoftiramisu@gmail.com>
2014-05-19 21:17:40 +0300

    FrogRiverOne added

--- 2. Counting Elements/Counting Elements/Counting Elements/main.c
+++ 2. Counting Elements/Counting Elements/Counting Elements/main.c
@@ -13,7 +13,7 @@
 // PermCheck
 // Check whether array A is a permutation.
 // 100 out of 100 points on Codility
-int solution(int A[], int N) {
+int PermCheck(int A[], int N) {
     
     int *arr = calloc(N, sizeof(int));
     int i;
@@ -35,19 +35,55 @@ int solution(int A[], int N) {
     return 1;
 }
 
+// FrogRiverOne
+// Find the earliest time when a frog can jump to the other side of a river.
+// 100 out of 100 points on Codility
+int FrogRiverOne(int X, int A[], int N) {
+    
+    int i, jmp = 0;
+    int arr[X];
+    
+    for (i = 0; i < X; i++)
+    {
+        arr[i] = -1;
+    }
+    
+    for (i = 0; i < N; i++){
+        
+        if (arr[A[i] - 1] == -1) {
+            arr[A[i] - 1] = i;
+            jmp +=1;
+            
+            if (jmp == X) {
+                return i;
+            }
+        }
+    }
+    
+    return -1;
+    
+}
+
+
 
 int main(int argc, const char * argv[])
 {
     
     
     
-    int A[] = {4,1, 3, 2};
+    int D[] = {4,1, 3, 2};
     int B[] = {4, 1, 3};
     int C[] = {2147483646};
     
-    printf("Permutation: %i\n", solution(A, 4));
-    printf("Permutation: %i\n", solution(B, 3));
-    printf("Permutation: %i\n", solution(C, 1));
+    printf("Permutation: %i\n", PermCheck(D, 4));
+    printf("Permutation: %i\n", PermCheck(B, 3));
+    printf("Permutation: %i\n", PermCheck(C, 1));
+    
+    int A[8] = {1, 3, 1, 4, 2, 3, 5, 4};
+    
+    printf("Frog jump time: %i\n", FrogRiverOne(5, A, 8));
+    
+    
     
     
 }
