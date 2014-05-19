f3d459fd7bc2c75602f70331be2685ac656ad662
Denys Khlivnyy <testoftiramisu@gmail.com>
2014-05-20 00:19:57 +0300

    Added solution for "MaxCounters" problem

--- 2. Counting Elements/Counting Elements/Counting Elements/main.c
+++ 2. Counting Elements/Counting Elements/Counting Elements/main.c
@@ -9,6 +9,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Results {
+    int * C;
+    int L;
+};
+
 
 // PermCheck
 // Check whether array A is a permutation.
@@ -60,6 +65,53 @@ int FrogRiverOne(int X, int A[], int N) {
     
 }
 
+// MaxCounters
+// Calculate the values of counters after applying all alternating operations:
+// - increase counter by 1;
+// - set value of all counters to current maximum.
+// 100 out of 100 points on Codility
+struct Results MaxCounters(int N, int A[], int M) {
+    
+    struct Results result;
+    int *counter = calloc(N,sizeof(int));
+    
+    int i = 0, base_value = 0, max_value = 0;
+    
+    for(i = 0; i < N; i++)
+    {
+        counter[i] = 0;
+    }
+    
+    for(i = 0; i < M; i++){
+        if(A[i] <= N){
+            if(counter[A[i] - 1] < base_value){
+                counter[A[i] - 1] = base_value + 1;
+            }
+            else {
+                counter[A[i] - 1] +=1;
+            }
+            if(counter[A[i] - 1] > max_value){
+                max_value = counter[A[i] - 1];
+            }
+        }
+        if (A[i] > N){
+            base_value = max_value;
+        }
+    }
+    
+    for(i = 0; i < N; i++){
+        if(counter[i] < base_value){
+            counter[i] = base_value;
+        }
+    }
+
+    
+    result.C = counter;
+    result.L = N;
+    return result;
+}
+
+
 int main(int argc, const char * argv[])
 {
     // PermCheck tests:
@@ -75,5 +127,16 @@ int main(int argc, const char * argv[])
     int A[8] = {1, 3, 1, 4, 2, 3, 5, 4};
     printf("Frog jump time: %i\n", FrogRiverOne(5, A, 8));
     
+    // MaxCounters test:
+    int F[] = {3, 4, 4, 6, 1, 4, 4};
+    struct Results s = MaxCounters(5, F, 7);
+    
+    for (int i = 0; i < s.L; i++)
+    {
+        printf("%i, ", (s.C[i]));
+    }
+    
+    printf("\n");
+    return 0;
 }
 
